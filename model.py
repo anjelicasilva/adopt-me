57320490027f6640b4eb778e5ed7c8cb3ee8fd34
linh <thuytpham9@gmail.com>
2020-05-17 13:57:12 -0700

    import Flask library and set up config

---
+++ model.py
@@ -0,0 +1,49 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+
+db = SQLAlchemy()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def connect_to_db(app, db_uri="postgresql:///adoptme_db"):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+
+    connect_to_db(app)
+    print("Connected to DB.")
+

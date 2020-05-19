c83c95721657f62ef578f6e9280c799fb38aa870
linh <thuytpham9@gmail.com>
2020-05-19 16:42:34 -0700

    Modified all the tables

--- model.py
+++ model.py
@@ -1,60 +1,92 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from datetime import datetime, date
+from wekzeug.security import generate_password_hash, check_password_hash
 
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    """Store informtion about each user"""
 
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    favorite_id = db.Column(db.Integer, db.ForeignKey("favorites.favorite_id"))
     email = db.Column(db.String(30), nullable=True, unique = True)
     password = db.Column(db.String(100), nullable=True)
     home_location = db.Column(db.String(100), nullable=False)
 
-
-    companies = db.relationship("Company",
-                                backref="users",
-                                secondary="user_companies")
+    favorites = db.relationship("Favorite",
+                                backref="users")
 
     def __repr__(self):
         """Provide user's information in a helpful format"""
+        return f"User("{self.first_name}", "{self.last_name}", "{self.email}")"
 
-        return f"<User user_id={self.user_id}>"
+    def to_dict(self):
+        return {"user_id": self.user_id,
+                "email": self.email,
+                "password": self.password,
+                "home_location": self.home_location
+                }
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
-class FAVORITE(db.Model):
+class Favorite(db.Model):
+    """Store favorite pets for each user"""
 
     ___tablename__ = "favorites"
 
     favorite_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    dogs_id_list = 
+    dogs_id_list = db.Column()
 
-class SHELTER(db.Model):
+    def __repr__(self): 
+        
+       return f"User("{self.dogs_id_list}")"
+
+class Shelter(db.Model):
+    """Store shelter information"""
 
     ___tablename__ = "shelters"
 
     shelter_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     shelter_name = db.Column(db.String(30), nullable=True, unique = True)
     shelter_location = db.Column(db.String(100), nullable=False)
-    hours = db.Column(db.String(100), nullable=False)
+    operation_hour = db.Column(db.String, nullable=False, default=datetime.today())
+
+    def __repr__(self): 
+        
+        return f"Shelter("{self.shelter_name}", "{self.shelter_location}", \
+                "{self.operation_hour}")"
+
 
 class DOG(db.Model):
+    """Store dog information in each shelter"""
 
     __tablename__ = "dogs"
 
     dog_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    shelter_id = db.Column(db.Integer, db.ForeignKey("shelters.shelter_id"))
     dob_breed = db.Column(db.String(30), nullable=True, unique = True)
-    dog_photo = db.Column(db.String(30), nullable=True, unique = True)
+    dog_photo_url = db.Column(db.String(1000), nullable=False)
     description = db.Column(db.String(200), nullable=True)
     gender = db.Column(db.String(30), nullable=True, unique = True)
     age = db.Column(db.Integer, nullable=True)
     fixed = db.Column(db.String(30), nullable=True, unique = True)
-    
 
+    shelters = db.relationship("Shelter", backref="dogs")
 
+    def __repr__(self): 
 
+        return f"DOG("{self.dog_breed}", "{self.dog_photo_url}", \
+                "{self.description}", "{self.gender}", "{self.gender}", \
+                "{self.age}", "{self.fixed}")"
 
 
 

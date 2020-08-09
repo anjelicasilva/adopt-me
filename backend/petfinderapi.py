038b7347647fc791466233b57e9ca27ff31ab0f1
dominiquecuevas <cuevasdominique@gmail.com>
2020-08-08 19:03:24 -0700

    todo parse through pagination in api request; kwargs

--- backend/petfinderapi.py
+++ backend/petfinderapi.py
@@ -14,22 +14,32 @@ def get_token():
     token = res['token_type'] + ' ' + res['access_token']
     return token
 
-def get_response_data(url, token, payload=None):
+def get_response_data(url, token, **payload):
     headers = {'Authorization': token}
+    # if not payload:
+    #     payload = {}
     response = requests.get(url, headers=headers, params=payload)
     data = response.json()
+    # Todo: pagination, handle rate limit of 1000/day and 50/second
+    # if 'pagination' in data and data['pagination'].get('current_page')!=data['pagination'].get('total_pages'):
+    #     payload['page'] = data.get('current_page',0) + 1
+    #     token = token()
+    #     data = get_response_data(url, token=token, payload=params)
+    #     data = response.json()
     return data
 
 def get_animals(id=None, **kwargs):
     token = get_token()
+    print('kwargs:', kwargs)
     url = url_base + "/v2/animals"
     if id:
         url = url + f"/{id}"
     return get_response_data(url, token, payload=kwargs)
 
-def get_organization(href):
+def get_organizations(**kwargs):
     token = get_token()
-    url = url_base + href
+    if 'href' in kwargs:
+        url = url_base + kwargs.get('href')
+    else:
+        url = url_base + "/v2/organizations"
     return get_response_data(url, token)
-
-pprint(get_organization("/v2/organizations/ia36"))
